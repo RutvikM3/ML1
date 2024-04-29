80b11d9136e63072e1c162b441d51143a396d321
RutvikM3 <76453235+RutvikM3@users.noreply.github.com>
2024-04-29 02:37:33 -0400

    Update ML1.py

--- ML1.py
+++ ML1.py
@@ -6,12 +6,13 @@ import matplotlib.image as mpimg
 # read the image 
 img = cv2.imread('img000220_3.jpg') 
 sh = img.shape
-# print(sh)
-
 center = np.array([((sh[0] - 1) / 2), ((sh[1] - 1) / 2)])
 
-"""
-# Total non-space pixels
+
+# Count total non-space pixels
+# Treates pixel vals below 20 as negligible
+# Data cleaning
+
 total = 0
 # cutoff val
 k = 20
@@ -22,14 +23,16 @@ for i in range(sh[0]):
             total += 1
             
 print(total, sh[0]*sh[1])
-"""
 
-# Plotting assist
+# Function that assist with plotting the ML method iterations
 def get_rectangle(center, width, height, rot):
+    # Calculate rectangle four corners (top, bottom, left, right)
     tl = np.array([-width/2, height/2])
     tr = np.array([width/2, height/2])
     bl = np.array([-width/2, -height/2])
     br = np.array([width/2, -height/2])
+
+    # Rotate coordinates by angle
     rot_mat = np.array([[np.cos(rot), -np.sin(rot)],
                [np.sin(rot), np.cos(rot)]])
     tl1 = rot_mat @ tl
@@ -37,6 +40,7 @@ def get_rectangle(center, width, height, rot):
     bl1 = rot_mat @ bl
     br1 = rot_mat @ br
 
+    # Invert for plotting
     bl1[1] *= -1
     br1[1] *= -1
     tl1[1] *= -1
@@ -62,14 +66,13 @@ def get_rectangle(center, width, height, rot):
 
     return np.concatenate((xs1,xs2,xs3,xs4)), np.concatenate((ys1,ys2,ys3,ys4)), [tl1+center,tr1+center,bl1+center,br1+center]
 
-
+# Load in image and define tuning hyperparameter
 img = mpimg.imread('img000220_3.jpg')
-
-
 C = 1/2e8  # Tuning hyperparameter
 
-# Objective function: largest sc/space ratio + rectangle size
+# Objective function used in ML method
 def get_obj(corners, width, height):
+    # Get corner coordinates and slopes
     tl1 = corners[0]
     tr1 = corners[1]
     m1 = (tr1[1]-tl1[1])/(tr1[0]-tl1[0])
@@ -77,7 +80,7 @@ def get_obj(corners, width, height):
     br1 = corners[3]
     m2 = (br1[1]-bl1[1])/(br1[0]-bl1[0])
 
-    # Total non-space pixels
+    # Total non-space pixels in rectangle
     total = 0
     # cutoff val
     k = 20
@@ -96,20 +99,22 @@ def get_obj(corners, width, height):
     return ratio - C * (size - 26000) ** 2
 
 
+# Define ML method and plotting parameters
 plot = True
 
 width = 400
 height = 400
 
-sf = 5
+sf = 5  # Width/height scale factor
 
 rot = 0*np.pi/180 # 0 radians
 
-rf = 10 * np.pi/180
+rf = 10 * np.pi/180  # Rotation scale factor
 
-step = 100
-rstep = 4
+step = 100  # Gradient descent step size
+rstep = 4   # Gradient descent rotation step size
 
+# Plot spacecraft
 imgplot = plt.imshow(img)
 xs, ys, corners = get_rectangle(center, width, height, rot)
 obj = get_obj(corners, width, height)
@@ -117,21 +122,26 @@ plt.scatter(xs, ys, c='r')
 plt.pause(0.05)
 plt.clf()
 
+# Start ML Method (100 iterations max)
 for i in range(100):
     print()
     print("Iteration", i+1)
+
+    # Gradient in width direction
     xw, yw, cw = get_rectangle(center, width + sf, height, rot)
     objw = get_obj(cw, width + sf, height)
     xw2, yw2, cw2 = get_rectangle(center, width - sf, height, rot)
     objw2 = get_obj(cw2, width - sf, height)
     gw = (objw-objw2)/(2*sf)
 
+    # Gradient in height direction
     xh, yh, ch = get_rectangle(center, width, height + sf, rot)
     objh = get_obj(ch, width, height + sf)
     xh2, yh2, ch2 = get_rectangle(center, width, height - sf, rot)
     objh2 = get_obj(ch2, width, height - sf)
     gh = (objh-objh2)/(2*sf)
 
+    # Gradient in rotation direction
     xr, yr, cr = get_rectangle(center, width, height, rot + rf)
     objr = get_obj(cr, width, height)
     xr2, yr2, cr2 = get_rectangle(center, width, height, rot - rf)
@@ -139,13 +149,15 @@ for i in range(100):
     gr = (objr-objr2)/(2*rf)
 
     print(gw, gh, gr)
-    
+
+    # Update estimates for this iteration
     width += step * gw
 
     height += step * gh
 
     rot += rstep * gr
-    
+
+    # get new objective function value and plot
     imgplot = plt.imshow(img)
     xs, ys, corners = get_rectangle(center, width, height, rot)
     obj = get_obj(corners, width, height)
